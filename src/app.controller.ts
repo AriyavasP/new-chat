8e37ca3dce22d6e736e69fa5b465232fa49ce959
ariyavas <ariyavas@gmail.com>
2024-03-18 01:25:16 +0700

    Add configuration files and initial code

---
+++ src/app.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+
+interface IOpenAIResponse {
+  status: HttpStatus;
+  message: string;
+}
+
+@Controller('/api/v1')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post('/openAI')
+  async getOpenAIResponse(
+    @Body('message') message: string,
+  ): Promise<IOpenAIResponse> {
+    try {
+      const chat = await this.appService.chatAI(message);
+      const response: IOpenAIResponse = {
+        status: HttpStatus.OK,
+        message: chat,
+      };
+      return response;
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: error.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
